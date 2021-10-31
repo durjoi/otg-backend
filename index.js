3a3dc19b8439382d8f30cdda0f61b21e2108e3a3
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2021-10-31 11:44:01 +0600

    Cors added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient } = require("mongodb");
 
 const express = require('express');
 require('dotenv').config();
+var cors = require('cors')
 
 const { ObjectId } = require('mongodb');
 
@@ -11,7 +12,9 @@ const { ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3005;
+
 app.use(express.json());
+app.use(cors())
 
 
 // Replace the uri string with your MongoDB deployment's connection string.
@@ -22,118 +25,118 @@ const client = new MongoClient(uri);
 
 
 async function run() {
-  try {
-    await client.connect();
-    const database = client.db("otg");
-    const eventsCollection = database.collection("events");
-    const bookingsCollection = database.collection("bookings");
-    
-    // Get All the Events
-    app.get('/events', async (req, res) => {
+    try {
+        await client.connect();
+        const database = client.db("otg");
+        const eventsCollection = database.collection("events");
+        const bookingsCollection = database.collection("bookings");
 
-        const events = await eventsCollection.find({})
-        
-        if(await events.count() == 0) {
-            console.log('No Result Found');
-        }
-        
-        events.toArray(function (err, result) {
-            if(err) {
-                res.send(err);
-            } else {
-                res.send(JSON.stringify(result));
+        // Get All the Events
+        app.get('/events', async(req, res) => {
+
+            const events = await eventsCollection.find({})
+
+            if (await events.count() == 0) {
+                console.log('No Result Found');
             }
+
+            events.toArray(function(err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(JSON.stringify(result));
+                }
+            });
+
         });
-        
-    });
 
-    // Get Event by id
-    app.get('/events/:id', async (req, res) => {
+        // Get Event by id
+        app.get('/events/:id', async(req, res) => {
 
-        const id = ObjectId(req.params.id);
+            const id = ObjectId(req.params.id);
 
-        const query = { _id: id };
+            const query = { _id: id };
 
-        const events = await eventsCollection.find(query);
-        
-        if(await events.count() == 0) {
-            console.log('No Result Found');
-        }
-        
-        events.toArray(function (err, result) {
-            if(err) {
-                res.send(err);
-            } else {
-                res.send(JSON.stringify(result));
+            const events = await eventsCollection.find(query);
+
+            if (await events.count() == 0) {
+                console.log('No Result Found');
             }
+
+            events.toArray(function(err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(JSON.stringify(result));
+                }
+            });
+
         });
-        
-    });
 
-    // Insert an Event
-    app.post('/events', async (req, res) => {
-        const newEvent = req.body;
-        const result = await eventsCollection.insertOne(newEvent);
-        console.log(`A document was inserted in Event Collection with the _id: ${result.insertedId}`);
+        // Insert an Event
+        app.post('/events', async(req, res) => {
+            const newEvent = req.body;
+            const result = await eventsCollection.insertOne(newEvent);
+            console.log(`A document was inserted in Event Collection with the _id: ${result.insertedId}`);
 
-        res.send(result.insertedId);
-    });
+            res.send(result.insertedId);
+        });
 
 
 
-    // Get all the Bookings
-    app.get('/bookings', async (req, res) => {
-        const bookings = await bookingsCollection.find({});
-        
-        if(await bookings.count() == 0) {
-            console.log('No Result Found');
-        }
+        // Get all the Bookings
+        app.get('/bookings', async(req, res) => {
+            const bookings = await bookingsCollection.find({});
 
-        bookings.toArray(function (err, result) {
-            if(err) {
-                res.send(err);
-            } else {
-                res.send(JSON.stringify(result));
+            if (await bookings.count() == 0) {
+                console.log('No Result Found');
             }
+
+            bookings.toArray(function(err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(JSON.stringify(result));
+                }
+            });
         });
-    });
 
-    // Insert new Booking
-    app.post('/bookings', async (req, res) => {
-        const newEvent = req.body;
-        const result = await bookingsCollection.insertOne(newEvent);
-        console.log(`A document was inserted in Booking Collection with the _id: ${result.insertedId}`);
+        // Insert new Booking
+        app.post('/bookings', async(req, res) => {
+            const newEvent = req.body;
+            const result = await bookingsCollection.insertOne(newEvent);
+            console.log(`A document was inserted in Booking Collection with the _id: ${result.insertedId}`);
 
-        res.send(result.insertedId);
-    });
+            res.send(result.insertedId);
+        });
 
 
-    // Get Bookings by User Id
-    app.get('/bookings/:userid', async (req, res) => {
-        const query = { user_id: parseInt(req.params.userid) };
+        // Get Bookings by User Id
+        app.get('/bookings/:userid', async(req, res) => {
+            const query = { user_id: parseInt(req.params.userid) };
 
-        const bookings = await bookingsCollection.find(query);
-        
-        if(await bookings.count() == 0) {
-            console.log('No Result Found');
-        }
-        
-        bookings.toArray(function (err, result) {
-            if(err) {
-                res.send(err);
-            } else {
-                res.send(JSON.stringify(result));
+            const bookings = await bookingsCollection.find(query);
+
+            if (await bookings.count() == 0) {
+                console.log('No Result Found');
             }
-        });
-        
-    });
 
-  } finally {
-    // await client.close();
-  }
+            bookings.toArray(function(err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(JSON.stringify(result));
+                }
+            });
+
+        });
+
+    } finally {
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`OTG Backend Server Listening at http://localhost:${port}`);
+    console.log(`OTG Backend Server Listening at http://localhost:${port}`);
 })
